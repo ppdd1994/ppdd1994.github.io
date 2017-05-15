81dc630f8ce1535631f681bc02ecfb15ba36a020
ppdd1994 <ppdd1994@sina.com>
2017-05-15 14:54:14 +0800

    add app work

--- js/controllers.js
+++ js/controllers.js
@@ -26,6 +26,13 @@ myApp.controller('workCtrl', function ($scope) {
             "demoUrl":"../link/htmlgame/index.html"
         },
         {
+            "name": "仿饿了么APP-商家页",
+            "intro": "使用Vue.js+es6+webpack技术，采用组件化、模块化的开发方式",
+            "imgUrl": "images/vue.jpg",
+            "codeUrl":"https://github.com/ppdd1994/-app",
+            "demoUrl":"http://vuejssellapp.t.imooc.io"
+        },
+        {
             "name": "响应式金融网站",
             "intro": "适应多终端",
             "imgUrl": "images/fin.png",
